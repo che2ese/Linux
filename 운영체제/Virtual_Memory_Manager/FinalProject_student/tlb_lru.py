3ca63294d675749f7abf0605437d89b6ec828123
MS_1129 <141838530+che2ese@users.noreply.github.com>
2024-06-15 18:57:41 +0900

    tlb_lru.py 업데이트

--- 운영체제/Virtual_Memory_Manager/FinalProject_student/tlb_lru.py
+++ 운영체제/Virtual_Memory_Manager/FinalProject_student/tlb_lru.py
@@ -1,15 +1,21 @@
-from global_constants import TLB_ENTRIES
-
-
-class TLBLru:
+class TLB_LRU:
     def __init__(self):
-        # TODO: 구현
+        self.tlb = []
+        self.access_order = []
 
-    def get_frame(self, page_number):
-        # TODO: 구현
-
-
+    def search(self, page_number):
+        for entry in self.tlb:
+            if entry[0] == page_number:
+                self.access_order.remove(page_number)
+                self.access_order.append(page_number)
+                return entry[1]
+        return -1  # TLB 미스
 
     def update(self, page_number, frame_number):
-        # TODO: 구헌
+        if len(self.tlb) >= 16:
+            oldest_page = self.access_order.pop(0)
+            self.tlb = [entry for entry in self.tlb if entry[0] != oldest_page]
+        self.tlb.append((page_number, frame_number))
+        self.access_order.append(page_number)
+
 
